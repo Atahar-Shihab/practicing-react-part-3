629cfdd1975f5754e8b530edd79bd5e72dd8e89c
Atahar Shihab <shihabatahar@gmail.com>
2026-09-21 16:32:42 +0600

    Installed necessary package modules

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Daisynav from './Components/DaisyNav/Daisynav'
+
+function App() {
+
+  return (
+    <>
+      
+    <header>
+      <Daisynav></Daisynav>
+    </header>
+    
+    
+    <main>
+            <h1 className="underline text-7xl text-center text-blue-500 font-bold mt-20">Get started</h1>
+
+    </main>
+    </>
+  )
+  }
+
+  export default App
