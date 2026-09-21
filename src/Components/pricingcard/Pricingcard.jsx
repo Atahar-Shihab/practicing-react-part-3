b0bae65342afd9c0b2269c0138dbbeadfbb7a34a
Atahar Shihab <shihabatahar@gmail.com>
2026-09-22 03:08:39 +0600

    added a design in the cards

--- src/Components/pricingcard/Pricingcard.jsx
+++ src/Components/pricingcard/Pricingcard.jsx
@@ -2,12 +2,23 @@
 const Pricingcard = ({ pricing }) => {
     console.log(pricing);
     return (
-        <div className="border-2 border-blue-500 rounded-lg p-5 m-5 text-center">
+        <div className="aura aura-gold border-2 border-blue-500 rounded-lg p-5 m-5 text-center">
 
             <div>
 
                 <h1 className="text-5xl font-bold">{pricing.name}</h1>
-                <h4 className="text-3xl font-semibold text-blue-500">{pricing.price}</h4>
+                <img src={pricing.image} alt="" />
+                <h4 className="mt-5 text-3xl font-semibold text-blue-500  ">{pricing.price}</h4>
+            </div>
+
+            <div className="mt-5">
+                <h3 className="text-2xl font-semibold">Features:</h3>
+                <p className="text-lg">Here are the features of this plan:</p>
+                <ol className="list-decimal list-inside text-center">
+                    {
+                        pricing.features.map((feature, index) => <li key={index}>{feature}</li>)
+                    }
+                </ol>
             </div>
             
 </div>
